4e9143b0be61e299cd6fe5ff340e9419be02930b
FlowerCode <FlowerCode@0GiNr.com>
2014-05-25 00:28:03 +0800

    + Make everything perform asynchronously via async library  + Automatically download / reload missing filters

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 "use strict";
 
-var http = require('http'),
+var fs = require('fs'),
+    http = require('http'),
     https = require('https'),
+    path = require('path'),
     _ = require('underscore'),
     httpProxy = require('http-proxy'),
     URI = require('URIjs'),
+    async = require('async'),
+    request = require('request'),
     matcher = require('./matcher').defaultMatcher,
     filterClasses = require('./filterClasses'),
-    Filter = filterClasses.Filter,
-    fs = require('fs');
+    Filter = filterClasses.Filter;
 
 // 1px transparent GIF from http://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif
 var transparentImage = '\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00\xFF\xFF\xFF\x21\xF9\x04\x01\x00\x00\x00\x00\x2C\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x01\x44\x00\x3B';
 
-var list;
+var filters = [
+    { 'name': 'EasyList China+EasyList', 'url': 'https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt' },
+    { 'name': 'Adblock Warning Removal List', 'url': 'https://easylist-downloads.adblockplus.org/antiadblockfilters.txt' }
+];
 
 function contentTypeFromRequest(req) {
     var accept = req.headers['accept'] || '';
@@ -52,23 +58,8 @@ function contentTypeFromRequest(req) {
     }
 }
 
-https.get({
-        host: 'easylist-downloads.adblockplus.org',
-        path: '/easylistchina+easylist.txt'
-    },
-
-    function (res) {
-        res.on('data', function (chunk) {
-            list += chunk;
-            console.log('Received ' + list.length / 1024 + ' KB');
-        });
-        res.on('end', function () {
-            startFiltering();
-        });
-    });
-
-function startFiltering() {
-    list = list.split('\n');
+function loadFilter(data) {
+    var list = data.split('\n');
     list.shift();
 
     do {
@@ -77,7 +68,85 @@ function startFiltering() {
             matcher.add(filter);
         }
     } while (list.length > 0);
+}
 
+async.waterfall([
+    function(callback) {
+      fs.mkdir('filters', null, function() {
+          callback(null);
+      })
+    },
+    function(callback) {
+        async.reject(filters, function(filter, callback) {
+            var filterPath = path.join('filters', filter['name']);
+            console.log('Trying to load local filter ' + filterPath);
+            fs.readFile(filterPath, 'utf-8', function(error, data) {
+                if (!error && data) {
+                    loadFilter(data);
+                    callback(true);
+                } else {
+                    callback(false);
+                }
+            });
+        }, function(result) {
+            callback(null, result);
+        });
+    },
+    function (filtersMissing, callback) {
+        async.each(filtersMissing, function(filter, callback) {
+            var filterPath = path.join('filters', filter['name']);
+            var url = filter['url'];
+
+            console.log('Downloading ' + url + ' to path ' + filterPath);
+            request.get(url)
+                .pipe(fs.createWriteStream(filterPath))
+                .on('finish', function() {
+                    console.log('Successfully downloaded ' + url);
+                    callback();
+                })
+                .on('error', function(error) {
+                    callback('Failed to download from url ' + url);
+                });
+        }, function(error) {
+            if (error) {
+                callback(error);
+            } else {
+                callback(null, filtersMissing);
+            }
+        });
+    },
+    function(filtersMissing, callback) {
+        async.reject(filtersMissing, function(filter, callback) {
+            var filterPath = path.join('filters', filter['name']);
+            console.log('Trying to reload missing filter ' + filterPath);
+            fs.readFile(filterPath, 'utf-8', function(error, data) {
+                if (!error && data) {
+                    loadFilter(data);
+                    callback(true);
+                } else {
+                    callback(false);
+                }
+            });
+        }, function(result) {
+            callback(null, result);
+        });
+    }, function(filtersMissing, callback) {
+        async.each(filtersMissing, function(filter, callback) {
+            console.error('Could not load filter' + filter['name']);
+        });
+
+        callback(null);
+    }
+    ], function (error, result) {
+        if (error) {
+            throw error;
+        } else {
+            startFiltering();
+        }
+    }
+);
+
+function startFiltering() {
     var proxy = new httpProxy.createProxyServer();
 
     proxy.on('error', function (e) {
